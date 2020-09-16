ef0744befa6c54de51a76e455a2f4de2de3394b3
Daniel <daniel@livup.com.br>
2020-09-16 11:48:18 -0300

    heroku config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app
-    .listen(process.env.PORT)
-    .then(() => console.log(`Server is listening on port ${process.env.PORT}`));
+    .listen(process.env.PORT || 3000)
+    .then(() =>
+      console.log(`Server is listening on port ${process.env.PORT || 3000}`),
+    );
 }
 bootstrap();
