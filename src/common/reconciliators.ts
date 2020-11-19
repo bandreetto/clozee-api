0ab5b70f8d43878af92645b980663b3be931bdab
Bruno Andreetto <bandreetto@hotmail.com>
2020-11-18 23:26:38 -0300

    Remove forgotten log

--- src/common/reconciliators.ts
+++ src/common/reconciliators.ts
@@ -3,12 +3,7 @@ export function reconciliateByKey<T extends keyof U, U>(
   keysArray: Array<U[T]>,
   objectsArray: U[],
 ) {
-  try {
-    return keysArray.map(keyValue =>
-      objectsArray.find(obj => obj[key] === keyValue),
-    );
-  } catch (err) {
-    console.log({ key, keysArray, objectsArray });
-    throw err;
-  }
+  return keysArray.map(keyValue =>
+    objectsArray.find(obj => obj[key] === keyValue),
+  );
 }
