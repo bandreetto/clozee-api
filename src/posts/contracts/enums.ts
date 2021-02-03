37c8f56702242a9269b63894682a6738400aa6f8
Bruno Andreetto <bandreetto@hotmail.com>
2021-02-03 13:00:32 -0300

    Add more sizes

--- src/posts/contracts/enums.ts
+++ src/posts/contracts/enums.ts
@@ -16,6 +16,14 @@ export enum SIZES {
   P = 'P',
   M = 'M',
   G = 'G',
+  s38 = '38',
+  s39 = '39',
+  s40 = '40',
+  s41 = '41',
+  s42 = '42',
+  s43 = '43',
+  s44 = '44',
+  s45 = '45',
 }
 
 registerEnumType(SIZES, {
