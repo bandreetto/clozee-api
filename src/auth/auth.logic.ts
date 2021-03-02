cb0524277ca194cfb5d729c51d57959f727adb2a
Bruno Andreetto <bandreetto@hotmail.com>
2021-03-01 23:03:19 -0300

    Fix auth logic

--- src/auth/auth.logic.ts
+++ src/auth/auth.logic.ts
@@ -2,13 +2,13 @@ import { AccessToken, RefreshToken, Token } from './contracts';
 import { TOKEN_TYPES } from './contracts/enums';
 
 export function isRefreshToken(token: Token): token is RefreshToken {
-  return token.header.typ === TOKEN_TYPES.REFRESH;
+  return token?.header?.typ === TOKEN_TYPES.REFRESH;
 }
 
 export function isAccessToken(token: Token): token is AccessToken {
-  return token.header.typ === TOKEN_TYPES.ACCESS;
+  return token?.header?.typ === TOKEN_TYPES.ACCESS;
 }
 
 export function isPreSignToken(token: Token): token is AccessToken {
-  return token.header.typ === TOKEN_TYPES.PRE_SIGN;
+  return token?.header?.typ === TOKEN_TYPES.PRE_SIGN;
 }
