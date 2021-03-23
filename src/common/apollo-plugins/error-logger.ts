ec74003a389ff833d5a5fa7b023922a24871e384
Bruno Andreetto <bandreetto@hotmail.com>
2021-03-23 10:14:42 -0300

    Remove unauthorized and forbiden errors log

--- src/common/apollo-plugins/error-logger.ts
+++ src/common/apollo-plugins/error-logger.ts
@@ -6,6 +6,11 @@ export const errorLoggerPlugin = {
   requestDidStart() {
     return {
       didEncounterErrors(context) {
+        if (
+          context.error[0].message === 'Unauthorized' ||
+          context.error[0].message === 'Forbidden resource'
+        )
+          return;
         logger.error({
           message: 'Graphql Error',
           errors: context.errors,
