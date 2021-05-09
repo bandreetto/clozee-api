d6e3a29c6bc8781eeb3d54bb3f22391ea767c49e
Bruno Andreetto <bandreetto@hotmail.com>
2021-05-09 20:14:22 -0300

    Charge wire transfer from clozee

--- src/common/contants.ts
+++ src/common/contants.ts
@@ -1,6 +1,7 @@
 export const MINIMUM_TRANSACTION_VALUE = 700;
 export const TAX_PERCENTAGE = 0.05;
 export const FIXED_TAX = 500;
+export const WIRE_TRANFER_TAX = 367;
 
 export enum ERROR_CODES {
   POST_ALREADY_SOLD,
