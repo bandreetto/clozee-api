b40254d4658fa8934aaad431be07872d230ac554
Bruno Andreetto <bandreetto@hotmail.com>
2021-06-01 14:33:45 -0300

    Lint fix

--- src/orders/orders.logic.ts
+++ src/orders/orders.logic.ts
@@ -5,36 +5,39 @@ export function getSubTotal(posts: Post[]): number {
 }
 
 export const getDonationPercentage = (variableTax: number, fixedTax: number, posts: Post[]): number => {
-  const pricesAfterClozeeTax = posts.map(p => [p.price * (1 - variableTax) - fixedTax, p.donationPercentage])
-  const weightedArithmeticMean = pricesAfterClozeeTax.reduce(
-    (acc, [sellerAmount, donationPercentage]) => acc + sellerAmount * (donationPercentage || 0),
-    0,
-  )/pricesAfterClozeeTax.reduce((acc, [price]) => acc + price, 0);
+  const pricesAfterClozeeTax = posts.map(p => [p.price * (1 - variableTax) - fixedTax, p.donationPercentage]);
+  const weightedArithmeticMean =
+    pricesAfterClozeeTax.reduce(
+      (acc, [sellerAmount, donationPercentage]) => acc + sellerAmount * (donationPercentage || 0),
+      0,
+    ) / pricesAfterClozeeTax.reduce((acc, [price]) => acc + price, 0);
 
-  return weightedArithmeticMean
-}
+  return weightedArithmeticMean;
+};
 
 export const getClozeeAmount = (variableTax: number, fixedTax: number, posts: Post[]): number => {
-  const subTotal = getSubTotal(posts)
-  const clozeeAmount = Math.floor(
-    subTotal * variableTax + posts.length * fixedTax,
-  );
+  const subTotal = getSubTotal(posts);
+  const clozeeAmount = Math.floor(subTotal * variableTax + posts.length * fixedTax);
   return clozeeAmount;
-}
+};
 
 export function getDonationAmount(variableTax: number, fixedTax: number, posts: Post[]): number {
   const subTotal = getSubTotal(posts);
   const clozeeAmount = getClozeeAmount(variableTax, fixedTax, posts);
   const donationPercentage = getDonationPercentage(variableTax, fixedTax, posts);
   const sellerAmount = subTotal - clozeeAmount;
-  const donationAmount = Math.floor(sellerAmount * (donationPercentage/100));
+  const donationAmount = Math.floor(sellerAmount * (donationPercentage / 100));
   return donationAmount;
 }
 
-export function getSplitValues(variableTax: number, fixedTax: number, posts: Post[], ): [clozeeSplit: number, sellerSplit: number] {
+export function getSplitValues(
+  variableTax: number,
+  fixedTax: number,
+  posts: Post[],
+): [clozeeSplit: number, sellerSplit: number] {
   const subTotal = getSubTotal(posts);
   const clozeeAmount = getClozeeAmount(variableTax, fixedTax, posts);
   const sellerAmount = subTotal - clozeeAmount;
   const donationAmount = getDonationAmount(variableTax, fixedTax, posts);
   return [clozeeAmount + donationAmount, sellerAmount - donationAmount];
-};
+}
