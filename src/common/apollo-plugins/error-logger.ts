af5e8289e73b60877e86fd1ecc163e1ca66eb5d3
Bruno Andreetto <bandreetto@hotmail.com>
2021-06-21 22:14:55 -0300

    Do not log error on user not found error

--- src/common/apollo-plugins/error-logger.ts
+++ src/common/apollo-plugins/error-logger.ts
@@ -6,13 +6,12 @@ export const errorLoggerPlugin = {
   requestDidStart() {
     return {
       didEncounterErrors(context) {
-        if (
-          context.errors[0].message === 'Unauthorized' ||
-          context.errors[0].message === 'Forbidden resource'
-        )
-          return;
+        if (context.errors[0].message === 'Unauthorized' || context.errors[0].message === 'Forbidden resource') return;
 
-        if (context.errors[0].message === 'This username already exists.') {
+        if (
+          context.errors[0].message === 'This username already exists.' ||
+          context.errors[0].message === 'User not found.'
+        ) {
           return logger.warn({
             message: 'Graphql Warning',
             warnings: context.errors,
